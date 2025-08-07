0beeb27d5e8091eb1ba56bd5d31c0affd5526c50
abdulbasitsuleiman <ask4bst@gmail.com>
2025-08-07 14:01:33 +0100

    Update res_partner.py
    feat: Add res.partner extension for Nigerian addresses

--- nigeria_lga/models/res_partner.py
+++ nigeria_lga/models/res_partner.py
@@ -12,22 +12,20 @@ class ResPartner(models.Model):
     nigeria_state_id = fields.Many2one('nigeria.state', string='Nigerian State')
     nigeria_lga_id = fields.Many2one('nigeria.lga', string='Local Government Area', 
                                      domain="[('state_id', '=', nigeria_state_id)]")
-    nigeria_ward_id = fields.Many2one('nigeria.ward', string='Ward',
-                                      domain="[('lga_id', '=', nigeria_lga_id)]")
     
     @api.onchange('nigeria_state_id')
     def _onchange_nigeria_state_id(self):
-        """Clear LGA and Ward when state changes"""
+        """Clear LGA when state changes"""
         if self.nigeria_state_id:
             self.nigeria_lga_id = False
-            self.nigeria_ward_id = False
             if not self.country_id:
                 nigeria = self.env['res.country'].search([('code', '=', 'NG')], limit=1)
                 if nigeria:
                     self.country_id = nigeria.id
     
-    @api.onchange('nigeria_lga_id')
-    def _onchange_nigeria_lga_id(self):
-        """Clear Ward when LGA changes"""
-        if self.nigeria_lga_id:
-            self.nigeria_ward_id = False
+    @api.onchange('country_id')
+    def _onchange_country_id(self):
+        """Clear Nigerian fields if country is not Nigeria"""
+        if self.country_id and self.country_id.code != 'NG':
+            self.nigeria_state_id = False
+            self.nigeria_lga_id = False
