205a4971fe5c4525909ed43b3ec9563ecc1cfeaf
abdulbasitsuleiman <ask4bst@gmail.com>
2025-08-07 07:47:40 +0100

    Create __init__.py
    feat: Add main init file

---
+++ nigeria_lga/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Supreme Stack Nigeria LGA Module. See LICENSE file for full copyright and licensing details.
+
+from . import models
+from . import controllers
+from . import wizards
