3103a3a8628f21bc64cc1c0172bef4f45a55b6e1
abdulbasitsuleiman <ask4bst@gmail.com>
2025-08-07 07:48:55 +0100

    Create __init__.py
    feat: Add models init file

---
+++ nigeria_lga/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2025 Supreme Stack
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import nigeria_state
+from . import nigeria_lga
+from . import nigeria_ward
+from . import res_partner
