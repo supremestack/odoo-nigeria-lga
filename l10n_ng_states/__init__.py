5213e8c4566690fb5385e55735aff4a627a3ed1d
abdulbasitsuleiman <ask4bst@gmail.com>
2025-08-08 11:51:17 +0100

    Update __init__.py

--- l10n_ng_states/__init__.py
+++ l10n_ng_states/__init__.py
@@ -1,12 +1,3 @@
 # -*- coding: utf-8 -*-
-# Copyright 2025 Supreme Stack
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
-
-"""
-Nigeria States and Local Government Areas Module
-
-Optimal structure:
-- States: Single file (37 records)
-- LGAs: Split by geopolitical zone and state (37 files)
-- Total: 774 Local Government Areas
-"""
+# Part of Supreme Stack Systems Limited
+# See LICENSE file for full copyright and licensing details.
